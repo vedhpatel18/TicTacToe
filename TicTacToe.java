c373ee70afdc102c98246f162e879f7ad0e09553
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 09:26:31 +0530

    UC4 Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,21 @@
 /* 
+@version 4.0
 @author Vedh
  */
-class TicTacToe {
-	public static void main(String[] args ){
-		
-		// This is "Hello App Program"
-		
-		String msg = "This is Dev Branch";
-		
-		System.out.println(msg);
-		
-	}
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        int slot = 7;
+
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
 }
