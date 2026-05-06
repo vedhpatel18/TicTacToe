2491a9e8e5353e60f3a665133c73b1327a418ff3
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 09:24:40 +0530

    UC3 Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,22 @@
 /* 
+@version 3.0
 @author Vedh
  */
-class TicTacToe {
-	public static void main(String[] args ){
-		
-		// This is "Hello App Program"
-		
-		String msg = "This is Dev Branch";
-		
-		System.out.println(msg);
-		
-	}
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+
+    // UC3: Reads slot number from user
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter slot number (1-9): ");
+        int slot = sc.nextInt();
+        return slot;
+    }
 }
