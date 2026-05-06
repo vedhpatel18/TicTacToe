109d3e85993777302cdb802dfad123b22fec5567
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 09:35:57 +0530

    UC9 Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,51 @@
 /* 
+@version 9.0
 @author Vedh
  */
-class TicTacToe {
-	public static void main(String[] args ){
-		
-		// This is "Hello App Program"
-		
-		String msg = "This is Dev Branch";
-		
-		System.out.println(msg);
-		
-	}
+public class TicTacToe {
+
+    static char[][] board = {
+        {'X', 'X', 'X'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(hasWon('X'));  // corrected call
+    }
+
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol &&
+                board[i][1] == symbol &&
+                board[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == symbol &&
+                board[1][i] == symbol &&
+                board[2][i] == symbol) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] == symbol &&
+            board[1][1] == symbol &&
+            board[2][2] == symbol) {
+            return true;
+        }
+
+        if (board[0][2] == symbol &&
+            board[1][1] == symbol &&
+            board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
 }
